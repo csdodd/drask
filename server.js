37e6baba4eb3473a4bb41a41609f09671bb5cf75
csdodd <colin@bakkenbaeck.no>
2016-09-30 10:03:29 +0200

    Listen on correct port for environment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/';
+var port = process.env.PORT || 3000;
 
 router.get("/",function(req,res){
   res.sendFile(path + "index.html");
@@ -21,6 +22,6 @@ app.use("*",function(req,res){
     console.log("Failed: " + req.originalUrl);
 });
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.listen(port, function(){
+  console.log("Live at Port " + port);
 });
