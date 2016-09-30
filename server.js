083e399116ab9804629afdc4c38f713576f357ff
csdodd <colin@bakkenbaeck.no>
2016-09-30 11:08:16 +0200

    Add Drake model and render it on face

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ router.get("/",function(req,res){
 });
 
 app.use('/css', express.static(__dirname + '/css'));
-app.use('/font-awesome', express.static(__dirname + '/font-awesome'));
-app.use('/fonts', express.static(__dirname + '/fonts'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/js', express.static(__dirname + '/js'));
-app.use('/less', express.static(__dirname + '/less'));
-app.use('/locales', express.static(__dirname + '/locales'));
+app.use('/ext_js', express.static(__dirname + '/ext_js'));
+app.use('/models', express.static(__dirname + '/models'));
+app.use('/media', express.static(__dirname + '/media'));
 
 app.use("/",router);
 
