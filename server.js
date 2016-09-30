9eb8ef873d2319c91c3068ed54638e0d7a1fc339
csdodd <colin@bakkenbaeck.no>
2016-09-30 09:59:13 +0200

    Empty template

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/';
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/font-awesome', express.static(__dirname + '/font-awesome'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/less', express.static(__dirname + '/less'));
+app.use('/locales', express.static(__dirname + '/locales'));
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+    console.log("Failed: " + req.originalUrl);
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
